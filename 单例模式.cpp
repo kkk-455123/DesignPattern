0338018e4f477876ebd7e228f7645bb8ef934129
kkk <82047973+kkk-455123@users.noreply.github.com>
2022-07-13 15:40:53 +0000

    补充了代码

--- 单例模式.cpp
+++ 单例模式.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    cout << "hello world" << endl;
+class Singleton
+{
+private:
+    Singleton() {
+        cout << "Singleton" << endl;
+    }
+    ~Singleton() {
+        cout << "~Singleton" << endl;
+    }
+    Singleton(const Singleton&);
+    Singleton& operator= (const Singleton&);
+
+public:
+    static Singleton* getInstance() {
+        static Singleton instance;
+        return &instance;
+    }
+};
+
+int main()
+{
+    Singleton* a = Singleton::getInstance();
+    Singleton* b = Singleton::getInstance();
     return 0;
 }
